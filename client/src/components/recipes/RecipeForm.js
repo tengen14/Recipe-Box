92d9e8248c41a08ba8d316d630f4969bac413fa4
Thane Engen <thaneengen@gmail.com>
2019-11-05 12:16:43 -0600

    Format Ingredient form data with flexbox

--- client/src/components/recipes/RecipeForm.js
+++ client/src/components/recipes/RecipeForm.js
@@ -5,15 +5,43 @@ import history from "../../history";
 class RecipeForm extends React.Component {
   renderField = ({ input, label, type, meta: { touched, error } }) => {
     return (
-      <div>
+      <React.Fragment>
         <label>{label}</label>
-        <div>
-          <input {...input} type={type} placeholder={label} />
-          {touched && error && <span>{error}</span>}
-        </div>
-      </div>
+        <input {...input} type={type} placeholder={label} />
+        {touched && error && <span>{error}</span>}
+      </React.Fragment>
     );
   };
+
+  // renderIngredients = ({ fields, meta: { error } }) => {
+  //   return (
+  //     <ul className="custom-field-array-container">
+  //       <li>
+  //         <button type="button" onClick={() => fields.push()}>
+  //           Add {!fields.length ? "Ingredient(s)" : "Another Ingredient"}
+  //         </button>
+  //       </li>
+  //       {fields.map((ingredient, index) => (
+  //         <li key={index} className="field-array-item">
+  //           <button
+  //             type="button"
+  //             title="Remove Ingredient"
+  //             onClick={() => fields.remove(index)}
+  //           >
+  //             <i class="fas fa-trash-alt"></i>
+  //           </button>
+  //           <Field
+  //             name={ingredient}
+  //             type="text"
+  //             component={this.renderField}
+  //             label={`Ingredient #${index + 1}`}
+  //           />
+  //         </li>
+  //       ))}
+  //       {error && <li className="error">{error}</li>}
+  //     </ul>
+  //   );
+  // };
 
   renderIngredients = ({ fields, meta: { error } }) => {
     return (
@@ -24,50 +52,21 @@ class RecipeForm extends React.Component {
           </button>
         </li>
         {fields.map((ingredient, index) => (
-          <li key={index} className="field-array-item">
-            <button
-              type="button"
-              title="Remove Ingredient"
-              onClick={() => fields.remove(index)}
-            >
-              &times;
-            </button>
+          <li
+            key={index}
+            className="ingredient-form-value field-array-item"
+          >
             <Field
               name={ingredient}
               type="text"
               component={this.renderField}
               label={`Ingredient #${index + 1}`}
             />
-          </li>
-        ))}
-        {error && <li className="error">{error}</li>}
-      </ul>
-    );
-  };
-
-  renderDirections = ({ fields, meta: { error } }) => {
-    return (
-      <ul className="custom-field-array-container">
-        <li>
-          <button type="button" onClick={() => fields.push()}>
-            Add {!fields.length ? "Direction(s)" : "Another Direction"}
-          </button>
-        </li>
-        {fields.map((direction, index) => (
-          <li key={index} className="field-array-item">
-            <button
-              type="button"
-              title="Remove Direction"
+            <i
+              title="Remove Ingredient"
               onClick={() => fields.remove(index)}
-            >
-              &times;
-            </button>
-            <Field
-              name={direction}
-              type="text"
-              component={this.renderField}
-              label={`Direction #${index + 1}`}
-            />
+              className="fas fa-trash-alt"
+            ></i>
           </li>
         ))}
         {error && <li className="error">{error}</li>}
@@ -83,18 +82,26 @@ class RecipeForm extends React.Component {
     return (
       <div>
         <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
-          <label>Recipe Title</label>
-          <Field
-            name="title"
-            component={this.renderField}
-            type="text"
-            placeholder="Recipe Title"
-          />
+          <div id="title-form">
+            <Field
+              id="title-form"
+              name="title"
+              component={this.renderField}
+              type="text"
+              label="Recipe Title"
+            />
+          </div>
           <FieldArray name="ingredients" component={this.renderIngredients} />
-          <FieldArray name="directions" component={this.renderDirections} />
-          <button className="ui button primary">Submit</button>
+          <div id="form-buttons">
+            <button className="ui button primary big">Submit</button>
+            <button
+              className="ui button negative big"
+              onClick={() => history.goBack()}
+            >
+              Cancel
+            </button>
+          </div>
         </form>
-        <button className="ui button negative" onClick={() => history.goBack()} >Cancel</button>
       </div>
     );
   }
@@ -104,7 +111,7 @@ const validate = values => {
   const errors = {};
 
   if (!values.title) {
-    errors.title = "Required";
+    errors.title = "! Required";
   }
 
   if (!values.ingredients || !values.ingredients.length) {
@@ -113,7 +120,7 @@ const validate = values => {
     const ingredientArrayErrors = [];
     values.ingredients.forEach((ingredient, index) => {
       if (!ingredient || !ingredient.length) {
-        ingredientArrayErrors[index] = "Required";
+        ingredientArrayErrors[index] = "! Required";
       }
     });
     if (ingredientArrayErrors.length) {
@@ -127,7 +134,7 @@ const validate = values => {
     const directionArrayErrors = [];
     values.directions.forEach((direction, index) => {
       if (!direction || !direction.length) {
-        directionArrayErrors[index] = "Required";
+        directionArrayErrors[index] = "! Required";
       }
     });
     if (directionArrayErrors.length) {
